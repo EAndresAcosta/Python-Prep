5b94e8e6b8487efd294fbe4d1232894c6485d73e
EAndresAcosta <andres921411@gmail.com>
2023-11-02 20:12:14 -0500

    Homework del modulo 05 completado

--- M04_flujosdecontrol/Prep_Course_Homework_04.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04.py
@@ -6,87 +6,137 @@
 # 1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla si es mayor o menor a cero
 
 # In[4]:
-
-
-
+num = [1, 2, 3, 4, 5]
+for i in num:
+    if i > 0:
+        print(i, "Es mayor a cero")
+    else:
+        print(i, "Es menor a cero")
 
 
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
 
 # In[5]:
-
-
-
+num = 2
+var = "2"
+if num == var:
+    print("Son el mismo tipo de dato")
+else:
+    print("No son el mismo tipo de dato")
 
 
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
 
 # In[7]:
-
-
-
+enteros = [2, 3, 4, 7, 8, 9, 10, 17, 18, 20]
+for i in enteros:
+    if i % 2 == 0:
+        print(i, "Es entero")
+    else:
+        print(i, "No es entero")
 
 
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
 
 # In[9]:
-
-
-
+for i in range(0, 6):
+    print("Valor", str(i), "elevado a la 3ra potencia", str(i ** 3))
 
 
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
 # In[10]:
-
-
-
+ciclo = 3
+for i in range(0, ciclo):
+    ciclo == 3
+    print(i)
 
 
 # 6) Utilizar un ciclo while para realizar el factoreo de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
 
 # In[33]:
-
-
-
+n = 7
+if type(n) == int and n > 0:
+    factorial = 1
+    while n > 0:
+        factorial *= n
+        n -= 1
+    print("El factorial es", factorial)
+else:
+    print(None)
 
 
 # 7) Crear un ciclo for dentro de un ciclo while
 
 # In[38]:
-
-
-
+num = 1
+while num < 5:
+    print("Ciclo while #" + str(num))
+    for i in range(1, num+1):
+        print("Ciclo for #" + str(i))
+    num += 1
 
 
 # 8) Crear un ciclo while dentro de un ciclo for
 
 # In[3]:
-
-
-
+n = 5
+for i in range(1, n):
+    print("Ciclo for " + str(i))
+    var = n
+    while (var > 0):
+        print("Ciclo while " + str(n))
+        n -= 1
+        if var >= 0:
+            break
 
 
 # 9) Imprimir los números primos existentes entre 0 y 30
 
 # In[54]:
-
-
+for numero in range(2, 31):
+    if numero <= 1:
+        print("0 y 1 no son numeros primos")
+        break
+    es_primo = True
+    for divisor in range(2, int(numero ** 0.5) + 1):
+        if numero % divisor == 0:
+            es_primo = False
+            break
+    if es_primo:
+        print(numero, " Es primo")
 
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
 
 # In[55]:
-
-
-
+for numero in range(2, 31):
+    if numero <= 1:
+        print("0 y 1 no son numeros primos")
+        break
+    es_primo = True
+    for divisor in range(2, int(numero ** 0.5) + 1):
+        if numero % divisor == 0:
+            es_primo = False
+            break
+    if es_primo:
+        print(numero, " Es primo")
 
 
 # 11) En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
 
 # In[56]:
-
-
+for numero in range(2, 31):
+    if numero <= 1:
+        print("0 y 1 no son numeros primos")
+        break
+    es_primo = True
+    for divisor in range(2, int(numero ** 0.5) + 1):
+        if numero % divisor == 0:
+            es_primo = False
+            break
+    if es_primo:
+        print(numero, " Es primo")
 
 
 # In[57]:
@@ -97,21 +147,45 @@
 # 12) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
 
 # In[62]:
-
-
-
+n = 100
+while n <= 300:
+    n += 1
+    if n % 12 != 0:
+        continue
+    print(n, " Es dividible por 12")
 
 
 # 13) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
 # In[73]:
-
-
+num = 1
+continuar = 1
+for numero in range(2, num):
+    if numero <= 1:
+        print("0 y 1 no son numeros primos")
+        break
+    for divisor in range(2, int(numero ** 0.5) + 1):
+        if numero % divisor == 0:
+            es_primo = False
+            break
+    if es_primo:
+        print(numero, " Es primo")
+        print("Desea encontrar el proximo numero primo")
+        if input() != 1:
+            print("Se finaliza el proceso")
+            break
+    else:
+        es_primo = True
+        num += 1
 
 
 # 14) Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
 
 # In[75]:
-
-
+num = 99
+while num < 300:
+    if num % 3 == 0 and num % 6 == 0:
+        print(num, "Es el primer numero divisible por 3 y multiplo de 6")
+        break
+    num += 1
 
--- M05_estructuradedatos/Prep_Course_Homework_05.py
+++ M05_estructuradedatos/Prep_Course_Homework_05.py
@@ -6,68 +6,53 @@
 # 1) Crear una lista que contenga nombres de ciudades del mundo que contenga más de 5 elementos e imprimir por pantalla
 
 # In[3]:
-
-
+ciudad = ['Colombia', 'Brasil', 'Argentina', 'Italia', 'Grecia', 'Estados unidos']
+print(ciudad)
 
 
 # 2) Imprimir por pantalla el segundo elemento de la lista
 
 # In[4]:
-
-
+print(ciudad[1])
 
 
 # 3) Imprimir por pantalla del segundo al cuarto elemento
 
 # In[8]:
-
-
-
+print(ciudad[1:4])
 
 
 # 4) Visualizar el tipo de dato de la lista
 
 # In[12]:
-
-
-
+type(ciudad)
 
 
 # 5) Visualizar todos los elementos de la lista a partir del tercero de manera genérica, es decir, sin explicitar la posición del último elemento
 
 # In[14]:
-
-
-
+print(ciudad[2:])
 
 
 # 6) Visualizar los primeros 4 elementos de la lista
 
 # In[15]:
-
-
-
-    
+print(ciudad[:4])
 
 
 # 7) Agregar una ciudad más a la lista que ya exista y otra que no ¿Arroja algún tipo de error?
 
 # In[16]:
-
-
-
-
-
-
-
+ciudad.append('Alemania')
+ciudad.append('Colombia')
+print(ciudad)
 
 
 # 8) Agregar otra ciudad, pero en la cuarta posición
 
 # In[20]:
-
-
-
+ciudad.insert(1, 'Canada')
+print(ciudad)
 
 
 # In[21]:
@@ -78,38 +63,33 @@
 # 9) Concatenar otra lista a la ya creada
 
 # In[22]:
-
-
+ciudad.extend(['Israel', 'Francia', 'Holanda'])
+print(ciudad)
 
 
 # 10) Encontrar el índice de la ciudad que en el punto 7 agregamos duplicada. ¿Se nota alguna particularidad?
 
 # In[23]:
-
-
-
+print(ciudad.index('Colombia'))
 
 
 # 11) ¿Qué pasa si se busca un elemento que no existe?
 
 # In[24]:
-
-
-
+print(ciudad.index('Rusia'))
 
 
 # 12) Eliminar un elemento de la lista
 
 # In[25]:
-
-
-
+ciudad.remove('Holanda')
+print(ciudad)
 
 
 # 13) ¿Qué pasa si el elemento a eliminar no existe?
 
 # In[27]:
-
+print(ciudad.remove('Holanda'))
 
 
 
@@ -117,92 +97,85 @@
 # 14) Extraer el úlimo elemento de la lista, guardarlo en una variable e imprimirlo
 
 # In[28]:
-
-
-
+elemento = ciudad.pop()
+print(elemento)
 
 
 # 15) Mostrar la lista multiplicada por 4
 
 # In[29]:
-
-
+print(ciudad * 4)
 
 
 # 16) Crear una tupla que contenga los números enteros del 1 al 20
 
 # In[32]:
-
-
+mi_tupla = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
 
 
 # 17) Imprimir desde el índice 10 al 15 de la tupla
 
 # In[35]:
-
-
+print(mi_tupla[10:16])
 
 
 # 18) Evaluar si los números 20 y 30 están dentro de la tupla
 
 # In[41]:
-
-
-
+print(20 in mi_tupla)
+print(30 in mi_tupla)
 
 
 # 19) Con la lista creada en el punto 1, validar la existencia del elemento 'París' y si no existe, agregarlo. Utilizar una variable e informar lo sucedido.
 
 # In[48]:
-
-
-
+elemento = 'Paris'
+if (not(elemento in ciudad)):
+    ciudad.append(elemento)
+    print('Se agrego el elemento')
+else:
+    print('El elemento ya existe en la lista')
 
 
 # 20) Mostrar la cantidad de veces que se encuentra un elemento específico dentro de la tupla y de la lista
 
 # In[51]:
-
-
-
+print(ciudad.count('Colombia'))
+print(mi_tupla.count(2))
 
 
 # 21) Convertir la tupla en una lista
 
 # In[52]:
-
-
-
+lista = list(mi_tupla)
+print(lista)
 
 
 # 22) Desempaquetar solo los primeros 3 elementos de la tupla en 3 variables
 
 # In[55]:
-
-
-
+a,b,c,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_ = mi_tupla
+print(a,b,c)
 
 
 # 23) Crear un diccionario utilizando la lista crada en el punto 1, asignandole la clave "ciudad". Agregar tambien otras claves, como puede ser "Pais" y "Continente".
 
 # In[57]:
-
-
-
-
+diccionario = {'ciudad': ciudad,
+                'paises':['Dinamarca', 'China',],
+                'Continente': ['America', 'Europa', 'Asia']
+                }
+print(diccionario)
 
 
 # 24) Imprimir las claves del diccionario
 
 # In[59]:
-
-
+print(diccionario.keys())
 
 
 # 25) Imprimir las ciudades a través de su clave
 
 # In[61]:
-
-
-
+print(diccionario.values())
 
